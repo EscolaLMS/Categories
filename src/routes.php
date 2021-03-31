43aa6df03ea66b2c71a5e4340b174ac5d9ceed26
Hubert Krzysztofiak <hubertkrzyszt@gmail.com>
2021-03-31 13:40:43 +0200

    Add middleware for routes

--- src/routes.php
+++ src/routes.php
@@ -3,7 +3,7 @@
 use EscolaLms\Categories\Http\Controllers\CategoryAPIController;
 use Illuminate\Support\Facades\Route;
 
-Route::group(['prefix' => 'api/categories'], function () {
+Route::group(['prefix' => 'api/categories', 'middleware' => \Illuminate\Routing\Middleware\SubstituteBindings::class], function () {
     Route::get('/', [CategoryAPIController::class, 'index']);
     Route::get('tree', [CategoryAPIController::class, 'tree']);
     Route::get('{category}', [CategoryAPIController::class, 'show']);
